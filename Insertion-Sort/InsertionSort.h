057dc4cdded670091b938ad34799021b2504e5c0
BBoldenow <BBoldenow@users.noreply.github.com>
2015-08-10 15:13:31 -0600

    Update InsertionSort.h

--- Insertion-Sort/InsertionSort.h
+++ Insertion-Sort/InsertionSort.h
@@ -7,13 +7,11 @@ void insertionSort(std::vector<T> &arr)
 	for (int i = 0; i < arr.size(); ++i)
 	{
 		T current = arr[i];
-		int pos = i;
 
-		while (pos > 0 && current < arr[pos - 1])
+		for (int pos = i; pos > 0 && current < arr[pos - 1]; --pos)
 		{
 			arr[pos] = arr[pos - 1];
 			arr[pos - 1] = current;
-			--pos;
 		}
 	}
 }
